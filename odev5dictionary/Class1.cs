05c15616fe10f580cd95d02d98a3397d0e255c26
serranurakmese <56965782+serranurakmese@users.noreply.github.com>
2021-03-13 16:29:17 +0300

    Add files via upload

---
+++ odev5dictionary/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace odev5dictionary
+{
+    class MyDictionary <I,S>
+    {
+        S[] items;
+        I[] items2;
+       
+
+        public MyDictionary()
+        {
+            items = new S[0];
+            items2 = new I[0];
+            
+        }
+
+        public void Add(S item, I item2)
+        {
+            S[] tempArray = items; //geçici dizinin referansı burada tutuluyor.
+            I[] tempArray2 = items2;
+            items = new S[items.Length + 1];
+            items2 = new I[items2.Length + 1];
+            //yeni ref oluşturunca adres uçmasın
+            //diye tempArray var.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+                items2[i] = tempArray2[i];
+            }
+
+            items[items.Length - 1] = item;
+            items2[items2.Length - 1] = item2;
+        }
+        public int Length
+        {
+            get { return items.Length; }
+            
+        }
+
+    }
+}
---
+++ odev5dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace odev5dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,string> names = new MyDictionary<string,string>();
+            names.Add("1","Engin");
+
+            Console.WriteLine(names.Length);
+         
+
+            names.Add("2","Kerem");
+
+            Console.WriteLine(names.Length);
+           
+            //new bellekte referans oluştur demek. örnek oluştur.
+        }
+    }
+}
